e793663c5611a1a625bee042ff4a32cf3c203924
Alexandre-SDV <alexandre.gouraud@supdevinci-edu.fr>
2025-03-16 10:59:34 +0100

    Import des Fonctions Filtres
    Import des Fonctions Filtres

--- Filtre_couleurs.py
+++ Filtre_couleurs.py
@@ -4,88 +4,31 @@ from PIL import Image
 import tkinter as tk
 from tkinter import filedialog
 
-# Demande le nom du fichier image
-print("Veuillez entrer le nom de l'image")
+# Importation des filtres
+import Filtre_jaune
+import Filtre_vert
+import Filtre_rouge
+import Filtre_noir_blanc
 
-#variable
-fichier_image = input()
-
-# Le fichier est défini par la variable fichier_image = input()
-print("Démarrage du traitement de : ", fichier_image)
-
-# Chargement du fichier image
-img = Image.open(fichier_image)
-
-# Dimension de l'image en largeur et en hauteur
-largeur_img, hauteur_img = img.size
-
-# Definition niveau blanc
-niveau_blanc = 180
-print("niveau_blanc",niveau_blanc)
-
-choixuser = input("Veuillez choisir une couleur de filtre pour votre image : 1 pour Jaune, 2 pour Vert, 3 pour Rouge et 4 pour Noir et Blanc : ")
+choixuser = input("Veuillez choisir une couleur de filtre pour votre image : 1 pour Jaune, 2 pour Vert, 3 pour Rouge, 4 pour Noir et Blanc et 5 pour faire les 4 filtres : ")
 
 if choixuser == "1":
-    print("Vous avez choisi le filtre Jaune")
-    for y in range(hauteur_img):
-        for x in range(largeur_img):
-            # traitements de l'image
-            r, v, b = img.getpixel((x, y))
-            # Si il y a des pixels blancs
-            if r > niveau_blanc and v > niveau_blanc and b > niveau_blanc:
-                # Je remplace le blanc par du JAUNE
-                img.putpixel((x, y), (254, 254, 1))
-            else:
-                img.putpixel((x, y), (b, v, r))
-    # Sauvegarde du fichier sous le nom de "résultat A"
-    img.save("Image Jaune.jpg")
-    print("Traitement terminé")
+    Filtre_jaune()
 
 elif choixuser == "2":
-    print("Vous avez choisi le filtre Vert")
-    for y in range(hauteur_img):
-        for x in range(largeur_img):
-            # traitements de l'image
-            r, v, b = img.getpixel((x, y))
-            # Si il y a des pixels blancs
-            if r > niveau_blanc and v > niveau_blanc and b > niveau_blanc:
-                # Je remplace le blanc par du VERT
-                img.putpixel((x, y), (145, 254, 1))
-            else:
-                img.putpixel((x, y), (r, b, v))
-    # Sauvegarde du fichier sous le nom de "résultat B"
-    img.save("Image Verte.jpg")
-    print("Traitement image verte terminé")
+    Filtre_vert()
 
 elif choixuser == "3":
-    print("Vous avez choisi le filtre Rouge")
-    for y in range(hauteur_img):
-        for x in range(largeur_img):
-            # traitements de l'image
-            r, v, b = img.getpixel((x, y))
-            # Si il y a des pixels blancs
-            if r > niveau_blanc and v > niveau_blanc and b > niveau_blanc:
-                # Je remplace le blanc par du ROUGE
-                img.putpixel((x, y), (254, 1, 1))
-            else:
-                img.putpixel((x, y), (b, r, v))
-    img.save("Image Rouge.jpg")
-    print("Traitement image rouge terminé")
+    Filtre_rouge()
     
 elif choixuser == "4":
-    print("Vous avez choisi le filtre Noir et Blanc")
-    for y in range(hauteur_img):
-        for x in range(largeur_img):
-            # traitements de l'image
-            r, v, b = img.getpixel((x, y))
-            # Si il y a des pixels blancs
-            if r > niveau_blanc and v > niveau_blanc and b > niveau_blanc:
-                # Je remplace le blanc
-                img.putpixel((x, y), (254, 254, 254))  # par du blanc
-            else:
-                img.putpixel((x, y), (0, 0, 0))
-    img.save("Image Noir et Blanc.jpg")
-    print("Traitement image noir et blanc terminé")
-
+    Filtre_noir_blanc()
+    
+elif choixuser == "5":
+    Filtre_jaune()
+    Filtre_vert()
+    Filtre_rouge()
+    Filtre_noir_blanc()
+    
 else:
     print("Vous n'avez pas choisi de filtre")
